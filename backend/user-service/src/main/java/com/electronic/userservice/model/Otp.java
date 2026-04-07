94839b4ac852900bab52b9b2c1320b10b944d3d1
Sog1n <siuace492@gmail.com>
2026-04-07 14:46:47 +0700

    Add order management functionality with JPA entities, services, and Kafka integration

---
+++ backend/user-service/src/main/java/com/electronic/userservice/model/Otp.java
@@ -0,0 +1,26 @@
+package com.electronic.userservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name = "otps")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Otp {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String loginKey;
+    private String code;
+    private LocalDateTime expiryTime;
+}
---
+++ backend/user-service/src/main/java/com/electronic/userservice/model/User.java
@@ -0,0 +1,27 @@
+package com.electronic.userservice.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+    private String email;
+    private String phone;
+    private String fullName;
+    private String address;
+    private String role = "USER"; // ADMIN, USER, SELLER
+
+}
